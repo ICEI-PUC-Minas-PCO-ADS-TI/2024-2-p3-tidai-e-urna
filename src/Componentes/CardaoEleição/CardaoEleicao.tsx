c9e7073140dfaf416d2e3a7d0eb1b385b557b489
Rafael <rbs13bg13@gmail.com>
2024-09-12 18:42:04 -0300

    Avatar Card Criado

--- src/Componentes/CardaoEleição/CardaoEleicao.tsx
+++ src/Componentes/CardaoEleição/CardaoEleicao.tsx
@@ -1,27 +1,27 @@
 import { Text, Avatar, VStack, Center, Box, Stack } from "native-base";
 import { Titulo } from "../Titulo/Titulo";
+import LogoPuc from "../../assets/puc.jpg";
 
+const imagemPathPuc = require("../../assets/puc.jpg");
 export function CardEleicao() {
   return (
-    <VStack w="100%" bg="blueGray.500" p={2} borderBottomRadius="lg" shadow={6}>
-      <Stack alignItems={"center"} textAlign={"center"} flexDir={"row"}>
-        <Titulo fontSize={20}>Curso:</Titulo>
-        <Text>ADS</Text>
+    <VStack
+      flexDir={"row"}
+      w="100%"
+      alignItems={"center"}
+      bg="blueGray.500"
+      p={2}
+      borderBottomRadius="lg"
+      shadow={6}
+    >
+      <Stack bg="white" justifyContent={"center"} alignItems={"center"} w={50}>
+        <Avatar source={imagemPathPuc}></Avatar>
       </Stack>
-      <Stack
-        space={10}
-        alignItems={"center"}
-        textAlign={"center"}
-        bg={"white"}
-        flexDir={"row"}
-      >
-        <Titulo fontSize={20}>Periodo:</Titulo>
-        <Text marginLeft={1} fontSize={20}>
-          3
+      <Stack marginLeft={2} alignItems={"center"} w={"100%"}>
+        <Text fontSize={20} fontWeight={"bold"} color={"white"}>
+          Terceiro Periodo
         </Text>
       </Stack>
-
-      <Text></Text>
     </VStack>
   );
 }
